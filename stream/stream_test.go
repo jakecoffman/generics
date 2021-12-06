30b433ea5a48f7444f4d2c902958573dbbc86e58
jake <jake@jakecoffman.com>
2021-12-06 14:36:54 -0600

    pretty up

--- stream/stream_test.go
+++ stream/stream_test.go
@@ -6,26 +6,33 @@ import (
 	"testing"
 )
 
-func TestStream_Some(t *testing.T) {
+func TestStream(t *testing.T) {
 	arr := []int{1, 12, 3}
-	stream := New(arr)
-	if !stream.Some(func(item *int) bool { return *item%2 == 0 }) {
-		t.Errorf("Expected true")
-	}
 
-	stream = New(arr)
-	stream = stream.Filter(func(item *int) bool { return *item%2 != 0 })
-	if stream.Some(func(item *int) bool { return *item%2 == 0 }) {
-		t.Errorf("Expected false")
-	}
+	t.Run("Some works", func(t *testing.T) {
+		stream := New(arr)
+		if !stream.Some(func(item *int) bool { return *item%2 == 0 }) {
+			t.Errorf("Expected true")
+		}
+	})
 
-	stream2 := Map(New(arr), func(t *int) string {
-		return strconv.Itoa(*t)
+	t.Run("Filtering", func(t *testing.T) {
+		stream := New(arr)
+		stream = stream.Filter(func(item *int) bool { return *item%2 != 0 })
+		if stream.Some(func(item *int) bool { return *item%2 == 0 }) {
+			t.Errorf("Expected false")
+		}
 	})
-	stream2 = stream2.Filter(func(item *string) bool {
-		return len(*item) == 1
+
+	t.Run("Mapping", func(t *testing.T) {
+		stream := Map(New(arr), func(t *int) string {
+			return strconv.Itoa(*t)
+		})
+		stream = stream.Filter(func(item *string) bool {
+			return len(*item) == 1
+		})
+		if !reflect.DeepEqual([]string{"1", "3"}, stream.ToSlice()) {
+			t.Error(stream.ToSlice())
+		}
 	})
-	if !reflect.DeepEqual([]string{"1", "3"}, stream2.ToSlice()) {
-		t.Error(stream2.ToSlice())
-	}
 }
